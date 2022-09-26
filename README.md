d10e35683adefe49e62dfda89baf1330307e5443
swxft <edenweb@outlook.com>
2022-09-26 14:13:26 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+JavaScript string library
--- src/index.js
+++ src/index.js
@@ -6,11 +6,36 @@ function capitalize (str) {
 }
 
 function allCaps (str) {
-    let capsLock = ''
-    for (let i = 0; i < str.length; i++){
-        capsLock += str[i].toUpperCase()
-    }
-    return capsLock
+    return str.toUpperCase()
 }
 
-console.log(capitalize('hello'))
+function capitalizeWords(str) {
+    const words = str.split(' ')
+    const capwords = words.map(capitalize)
+    return capwords.join(' ')
+}
+function capitalizeHeadline(str) {
+    const articles = ['the', 'in', 'a', 'an', 'and', 'but', 'for', 'at', 'by', 'from']
+    // loop through words in string (if index is not 0 and word is not in articles, capitalize)
+    //join words to return a sentence with capitalization
+    const sep = str.split(' ')
+    for (let item of sep) {
+        if (sep.indexOf(item) !== 0 && !item.includes(articles)) {
+            console.log(capitalize(item))
+        }
+    }
+    return
+}
+// console.log(allCaps('hello'))
+// console.log(capitalizeWords('hi im eden'))
+// console.log(capitalizeHeadline('From eden and reggie to a friend who goes by billie'))
+
+function samplefunc() {
+    const samp = ['once', 'there', 'was', 'a', 'purple', 'cow']
+    for (let word of samp) {
+        if (word.includes(samp)) {
+            console.log('true')
+        }
+    }
+}
+samplefunc()
