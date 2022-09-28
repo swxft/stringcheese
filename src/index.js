718aeaf7ceb3b2bb2adc43336daedb82e59b86b2
swxft <edenweb@outlook.com>
2022-09-28 13:25:49 -0400

    add capitalizedHeadline function

--- src/index.js
+++ src/index.js
@@ -16,26 +16,18 @@ function capitalizeWords(str) {
 }
 function capitalizeHeadline(str) {
     const articles = ['the', 'in', 'a', 'an', 'and', 'but', 'for', 'at', 'by', 'from']
-    // loop through words in string (if index is not 0 and word is not in articles, capitalize)
-    //join words to return a sentence with capitalization
     const sep = str.split(' ')
+    const newSentence = []
     for (let item of sep) {
-        if (sep.indexOf(item) !== 0 && !item.includes(articles)) {
-            console.log(capitalize(item))
+        if (sep.indexOf(item) !== 0 && !articles.includes(item)) {
+            newSentence.push(capitalize(item))
+        } else if (sep.indexOf(item) !== 0 && articles.includes(item)) {
+            newSentence.push(item)
         }
     }
-    return
+    newSentence.unshift(capitalizeWords(sep[0]));
+    return newSentence.join(' ')
 }
 // console.log(allCaps('hello'))
 // console.log(capitalizeWords('hi im eden'))
-// console.log(capitalizeHeadline('From eden and reggie to a friend who goes by billie'))
-
-function samplefunc() {
-    const samp = ['once', 'there', 'was', 'a', 'purple', 'cow']
-    for (let word of samp) {
-        if (word.includes(samp)) {
-            console.log('true')
-        }
-    }
-}
-samplefunc()
+console.log(capitalizeHeadline('from eden and reggie to a friend who goes by billie'))
